d08dac097f76965d2245239004423a9e269db124
zhuzewen <zhuzewen@yelighting.com>
2025-07-01 12:30:04 +0800

    添加udp解析和发送

--- ustack.c
+++ ustack.c
@@ -148,13 +148,94 @@ ehdr            iphdr           udphdr             payload
     return 0;
 }
 #else
-typedef void (*packet_handler_fn)(struct rte_mbuf *mbuf);
+typedef void (*packet_handler_fn)(struct rte_mempool *mbuf_pool, struct rte_mbuf *mbuf);
 
 struct protocol_handler{
     packet_handler_fn handler;
 };
 
-static void udp_handler_fun(struct rte_mbuf *mbuf)
+#define ENABLE_SEND 1
+
+#if ENABLE_SEND
+uint8_t g_src_mac[RTE_ETHER_ADDR_LEN];
+uint8_t g_dst_mac[RTE_ETHER_ADDR_LEN];
+
+uint32_t g_src_ip;
+uint32_t g_dst_ip;
+
+uint16_t g_src_port;
+uint16_t g_dst_port;
+#endif
+
+int ustack_encode_udp_pkt(uint8_t *msg, unsigned char *data, uint16_t length)
+{
+    struct rte_ether_hdr *ehdr = (struct rte_ether_hdr *)msg;
+    rte_memcpy(ehdr->s_addr, g_src_mac, RTE_ETHER_ADDR_LEN);
+    rte_memcpy(ehdr->d_addr, g_dst_mac, RTE_ETHER_ADDR_LEN);
+    ehdr->ether_type = htons(RTE_ETHER_TYPE_IPV4);
+
+    struct rte_ipv4_hdr *iphdr = (struct rte_ipv4_hdr *)(msg + sizeof(struct rte_ether_hdr));
+    /*字段解释：
+    Version（版本）：4位字段，表示IP协议的版本。IPv4的版本号是4，IPv6的版本号是6。
+    IHL（Internet Header Length）：4位字段，表示IP头的长度，以32位字为单位。最小值是5（20字节），
+    最大值是15,代表15个32字节（60字节）。
+    Type of Service（服务类型）：8位字段，用于指定数据包的优先级和服务质量。
+    Total Length（总长度）：16位字段，表示整个IP数据包的总长度，包括IP头和数据部分，以字节为单位。
+    Identification（标识）：16位字段，用于标识数据包的标识符，通常用于分片和重组。
+    Flags（标志）：3位字段，用于控制分片行为。包括是否允许分片（DF）和是否为最后一个分片（MF）。
+    Fragment Offset（片偏移）：13位字段，表示分片在原始数据包中的偏移量。
+    TTL（生存时间）：8位字段，表示数据包在网络中的最大生存时间，以跳数为单位。
+    Protocol（协议）：8位字段，表示数据包的上层协议类型，如TCP（6）、UDP（17）等。
+    Header Checksum（头部校验和）：16位字段，用于校验IP头的完整性。
+    Source Address（源地址）：32位字段，表示数据包的源IP地址。
+    Destination Address（目的地址）：32位字段，表示数据包的目的IP地址。
+    Options（选项）：可变长度字段，用于提供额外的选项，如路由信息等。
+    Padding（填充）：用于确保IP头的长度是32位的倍数。*/
+    iphdr->version_ihl = 0x45;/*版本4，头部长度20字节*/
+    iphdr->type_of_service = 0;
+    iphdr->total_length = htons(length - sizeof(struct rte_ether_hdr));
+    iphdr->packet_id = 0;
+    iphdr->fragment_offset = 0;
+    iphdr->next_proto_id = IPPROTO_UDP;
+    iphdr->time_to_live = 64;
+    iphdr->header_checksum = 0;
+    iphdr->dst_addr = g_dst_ip;
+    iphdr->src_addr = g_src_ip;
+    iphdr->header_checksum = rte_ipv4_cksum(iphdr);
+
+    struct rte_udp_hdr *udphdr = (struct rte_udp_hdr *)(msg + sizeof(struct rte_ether_hdr) + sizeof(struct rte_ipv4_hdr));
+    udphdr->src_port = g_src_port;
+    udphdr->dst_port = g_dst_port;
+    uint16_t udp_len = length - sizeof(struct rte_ether_hdr) - sizeof(struct rte_ipv4_hdr);
+    udphdr->dgram_len = htons(udp_len);
+    rte_memcpy((char*)(udphdr + 1), data, udp_len - sizeof(struct rte_udp_hdr));
+    
+    udphdr->dgram_cksum = 0;
+    udphdr->dgram_cksum = rte_ipv4_udptcp_cksum(iphdr, udphdr);
+    return length;
+}
+
+struct rte_mbuf* ustack_send(struct rte_mempool *mbuf_pool, unsigned char *data, uint16_t length)
+{
+    const unsigned total_length = length + sizeof(struct rte_ether_hdr) + 
+    sizeof(struct rte_ipv4_hdr) + sizeof(struct rte_udp_hdr);
+
+    struct rte_mbuf *mbuf = rte_pktmbuf_alloc(*mbuf_pool);
+
+    if(mbuf == NULL){
+        rte_exit(EXIT_FAILURE, "rte pktmbuf alloc failed\n");
+    }
+
+    mbuf.pkt_len = total_length;
+    mbuf.data_len = length;
+
+    uint8_t *pktdata = rte_pktmbuf_mtod(mbuf, uint8_t*);
+    ustack_encode_udp_pkt(pktdata, data, total_length);
+
+    return mbuf;
+}
+
+static void udp_handler_fun(struct rte_mempool *mbuf_pool, struct rte_mbuf *mbuf)
 {
     /*获取以太网头，rte_pktmbuf_mtod将mbuf转化为参数指定的数据结构*/
     struct rte_ether_hdr *ehdr = rte_pktmbuf_mtod(mbuf, struct rte_ether_hdr *);
@@ -175,6 +256,21 @@ static void udp_handler_fun(struct rte_mbuf *mbuf)
         struct rte_udp_hdr *udphdr = (struct rte_udp_hdr *)(iphdr + 1);/*跳过IP头，获取UDP头起始位置*/
         uint16_t length = udphdr->dgram_len; /*获取UDP数据包长度*/
         printf("length: %d, content: %s\n", length, (char *)(udphdr + 1)); /*获取UDP数据部分起始位置并打印*/
+        /*这里length是网络字节序，实际数值为udp头8字节加数据长度*/
+        uint16_t length = ntohs(udphdr->dgram_len) - sizeof(struct rte_udp_hdr);
+        
+#if ENABLE_SEND
+        /*发送回包*/
+        rte_memcpy(g_dst_mac, ehdr->s_addr, RTE_ETHER_ADDR_LEN);
+        rte_memcpy(g_src_mac, ehdr->d_addr, RTE_ETHER_ADDR_LEN);
+        rte_memcpy(&g_dst_ip, iphdr->src_addr, sizeof(uint32_t));
+        rte_memcpy(&g_src_ip, iphdr->dst_addr, sizeof(uint32_t));
+        rte_memcpy(&g_dst_port, &udphdr->dst_port, sizeof(uint16_t));
+        rte_memcpy(&g_src_port, &udphdr->src_port, sizeof(uint16_t));
+        ustack_send(mbuf_pool, (char *)(udphdr + 1), length);
+        
+#endif
+    
     }
 }
 
@@ -193,17 +289,17 @@ void register_handler(struct protocol_handler *handler)
     }
 }
 
-void process_packet(struct rte_mbuf **mbufs, uint16_t num_packets)
+void process_packet(struct rte_mempool *mbuf_pool, struct rte_mbuf **mbufs, uint16_t num_packets)
 {
     for(int i = 0; i < num_packets; i++){
         for(int h = 0; h < handler_count; h++){
-            handlers[h]->handler(mbufs[i]);
+            handlers[h]->handler(mbuf_pool, mbufs[i]);
         }
         rte_pktmbuf_free(mbufs[i]);
     }
 }
 
-void init_dpdk(int argc, char*argv[])
+void init_dpdk(int argc, char*argv[], struct rte_mempool **mbuf_pool)
 {
     // 初始化DPDK环境
     /*初始化Environment Abstraction Layer （EAL）*/
@@ -232,7 +328,7 @@ void init_dpdk(int argc, char*argv[])
     *   RTE_MBUF_DEFAULT_BUF_SIZE参数指定了每个缓冲区的默认大小（2048 + 128）字节，128位dpdk mbuf头部开销
     *   rte_socket_id()函数用于获取当前线程所在的NUMA节点，然后在该节点上分配内存池
     */
-    struct rte_mempool *mbuf_pool = rte_pktmbuf_pool_create("mbufpool", NUM_MBUFS, 0/*cache size*/, 0/*private size*/, RTE_MBUF_DEFAULT_BUF_SIZE/*pkt_room_size*/, rte_socket_id()/* `rte_socket_id()` 来获取当前线程所在的NUMA节点，然后在该节点上分配内存池*/);
+    *mbuf_pool = rte_pktmbuf_pool_create("mbufpool", NUM_MBUFS, 0/*cache size*/, 0/*private size*/, RTE_MBUF_DEFAULT_BUF_SIZE/*pkt_room_size*/, rte_socket_id()/* `rte_socket_id()` 来获取当前线程所在的NUMA节点，然后在该节点上分配内存池*/);
     if(!mbuf_pool){
         rte_exit(EXIT_FAILURE, "Could not create mbuf_pool\n");
     }
@@ -272,16 +368,23 @@ void init_application()
 
 int main(int argc, char *argv[])
 {
-    init_dpdk(argc, argv);
+    struct rte_mempool *mbuf_pool;
+    init_dpdk(argc, argv, &mbuf_pool);
     init_application();
     while(1){
         struct rte_mbuf *mbufs[BURST_SIZE];
+        /*
+        "burst"的含义：
+        突发接收：一次性尝试从网卡接收多个数据包（而不是单个包）
+        批处理：减少系统调用/硬件交互次数，提高吞吐量
+        零拷贝：数据直接从NIC（网卡）DMA到内存，避免内核拷贝
+        */
          unsigned int nb_received = rte_eth_rx_burst(g_dpdkd_port_id, 0/*rx id队列0*/, mbufs, BURST_SIZE);
         /*不应超过请求数量*/
         if(nb_received > BURST_SIZE){
             rte_exit(EXIT_FAILURE, "Error with rte_eth_rx burst\n");
         }
-        process_packet(mbufs, nb_received);
+        process_packet(mbuf_pool, mbufs, nb_received);
     }
     return 0;
 }
